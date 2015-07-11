2dfb3d847e0f8ee027e0997545e48ff5ab8a7d14
Anaibi <burgostatiana@yahoo.com>
2015-07-11 20:31:50 +0200

    clean up

--- acotar.js
+++ acotar.js
@@ -1,47 +1,68 @@
-$(window).load(function() {
+var cotas = [],
+    measures = [];
 
-  alert('enter main ref 1');
-  var cotas = []
-  
-  $('body')
-    .css('cursor', 'crosshair')
-    .on('click', function(e) { 
-      e.preventDefault();
-      acotar(e) })
-    .append('<div class="button">Finished</div><div class="output"></div>');
-
-  $('.button').on('click', function(e) {
-    setProportions();
-  })
-
-  function acotar(e) {
-    cotas.push(e.pageY);
-
+var ractive = new Ractive({
+  el: '#ractive-container',
+  template: '#template',
+  data: {
+    total: false,
+    measures: [],
+    counter: 0,
+    content: ''
   }
-  
-  function setProportions() {
-    var total = cotas[1] - cotas[0];
-
-    var partials = [],
-        proportions = [];
-
-    for (var i=3; i<cotas.length; i++) {
-      partials.push(cotas[i] - cotas[i-1]);
-      i++;
-    }
-  
-    for (var partial of partials) {
-     // proportions.push(+(partial / total).toFixed(2));
-      proportions.push(partial * 100 / total);
-    }
-
-    $('.output')
-      .append('<div class="main ref">Main ref: ' + total + '</div>')
-      .append('<div class="partials">Partials: ' + partials + '</div>')
-      .append('<div class="proportions">Proportion to main ref: ' + proportions + '</div>')
-
-  }
-
 });
 
+ractive.on('start', function(event) {
+  ractive.set({'content': 'Click two points to set main reference.'});
+});
 
+ractive.on('measure', function(event) {
+  
+  // add clicked position y
+  addCoordY(event.original);
+
+  // update counter
+  var counter = ractive.get('counter') + 1;
+  ractive.set('counter', counter);
+    
+  // set the firts two clicks to use as main ref
+  if (counter === 2) {
+    var total = cotas[1] - cotas[0];
+    ractive.set('total', total);
+  }
+
+  // each next pairs of clicks calculate partial and proportion
+  if ((counter > 2) && (counter % 2 == 0 )) {
+    addMeasure(counter);
+  }
+});
+
+ractive.on('remove', function(event) {
+  var i = (event.keypath).split('.')[1];
+  measures.splice(i, 1);
+});
+
+ractive.on('restart', function(event) {
+  cotas = [];
+  measures = [];
+  ractive.set({
+    total: 0,
+    counter: 0,
+    measures: measures,
+  });
+});
+
+    
+function addCoordY(e) {
+  cotas.push(e.pageY);
+}
+
+function addMeasure(i) { 
+  var measure = cotas[i-1] - cotas[i-2];
+  var proportion = +((measure * 100 / ractive.get('total')).toFixed(2));
+
+  measures.push({measure: measure, proportion: proportion});
+  ractive.set({
+    'measures': measures,
+  });
+}
